dbcdcab136cd06c30f847f61110735717ea9b525
Prem Kumar <v-prem.1.kumar@intra.riyadbank.com>
2024-01-20 21:41:27 +0530

    remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {TossCointContainer} from './app/modules/toss-coin';
